3e670f72c4c610157e7151fa7d83ff388e052498
Zsoltcsizmadia <163414210+Zsoltcsizmadia@users.noreply.github.com>
2024-12-10 12:48:44 +0100

    Add project files.

---
+++ 2024-12-09-footgolf/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _2024_12_09_footgolf
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Versenyző> versenyzok = new();
+            foreach (var sor in File.ReadAllLines("fob2016.txt"))
+            {
+                versenyzok.Add(new Versenyző(sor));
+            }
+            Console.WriteLine($"3. feladat: Versenyzők száma: {versenyzok.Count}");
+
+            var nok = 0;
+            
+            foreach (var no in versenyzok)
+            {
+                if (no.Nem == "Noi")
+                {
+                    nok++;
+                }
+            }
+            double arany = (versenyzok.Count / nok);
+            Console.WriteLine($"A nők száma: {nok}");
+            Console.WriteLine($"A női versenyzők aránya: {Math.Round(arany,2)}");
+        }
+    }
+}
---
+++ 2024-12-09-footgolf/Versenyző.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2024_12_09_footgolf
+{
+    internal class Versenyző
+    {
+        public string Név { get; set; }
+        public string Nem { get; set; }
+        public string EgyesületNév { get; set; }
+        public int PontSzám { get; set; }
+
+
+        public Versenyző(string sor)
+        {
+            string[] s = sor.Split(';');
+
+            Név = s[0];
+            Nem = s[1];
+            EgyesületNév = s[2];
+            PontSzám = int.Parse(s[3]);
+
+            
+        }
+    }
+}
